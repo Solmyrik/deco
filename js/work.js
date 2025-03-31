9ef86ad66190cf2524f28e9cff326ed36e109d2f
Dmitry <schedamian@gmail.com>
2025-03-31 17:49:51 +0300

    update work

--- js/work.js
+++ js/work.js
@@ -1,10 +1,15 @@
-const buttons = document.querySelectorAll('.work__pagination');
+const buttonsPagination = document.querySelectorAll('.work__pagination');
 const workItems = document.querySelectorAll('.work__items');
+
 let five = false;
 let activePagination = 0;
-buttons.forEach((button) => {
-  button.addEventListener('click', activeBtn);
-});
+
+if (buttonsPagination && buttonsPagination.length) {
+  buttonsPagination.forEach((button) => {
+    button.addEventListener('click', activeBtn);
+  });
+}
+
 function activeBtn(e) {
   let current = e.target;
   if (!current.classList.contains('active')) {
@@ -13,27 +18,31 @@ function activeBtn(e) {
       index = 2;
     }
 
-    if (five == true && index != 5 && index != 6) {
-      buttons[1].textContent = '02';
-      buttons[2].classList.remove('none');
-      buttons[5].classList.add('none');
+    if (five === true && index !== 5 && index !== 6) {
+      buttonsPagination[1].textContent = '02';
+      buttonsPagination[2].classList.remove('none');
+      buttonsPagination[5].classList.add('none');
       five = false;
     }
-    buttons[activePagination].classList.remove('active');
+
+    buttonsPagination[activePagination].classList.remove('active');
     current.classList.add('active');
     activePagination = index - 1;
     activeWork(activePagination);
+
     window.scrollTo({
       top: 0,
     });
-    if (index == 5) {
-      buttons[1].textContent = '...';
-      buttons[2].classList.add('none');
-      buttons[5].classList.remove('none');
+
+    if (index === 5) {
+      buttonsPagination[1].textContent = '...';
+      buttonsPagination[2].classList.add('none');
+      buttonsPagination[5].classList.remove('none');
       five = true;
     }
   }
 }
+
 function activeWork(index) {
   workItems.forEach((item) => {
     item.classList.remove('active');
